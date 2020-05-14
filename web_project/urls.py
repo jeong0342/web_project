3c2099a1d008c1c2abb193a7b0f818f2a1048bec
jeong0342 <arong0342@gmail.com>
2020-05-14 15:26:27 +0900

    hello

--- web_project/urls.py
+++ web_project/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-# from django.urls import path
+from django.urls import path
 
 # urlpatterns = [
 #     path('admin/', admin.site.urls),
@@ -22,5 +22,5 @@ from django.contrib import admin
 
 from django.urls import include
 urlpatterns = [
-    path("admin/",admin.site.urls, include("hello.urls")),
+    path('admin/', admin.site.urls, include("hello.urls")),
 ]
