32e807d52d46b2e4493a919d5d7a71f57453f6d4
jeong0342 <arong0342@gmail.com>
2020-05-15 09:26:13 +0900

    0515

--- hello/views.py
+++ hello/views.py
@@ -2,5 +2,27 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.http import JsonResponse
+
+class xxy1:
+    def __init__(self,x1,x2):
+        self.x1 = x1
+        self.x2 = x2
+    def getPredict(self):
+        y = self.x1 + self.x2
+        print('Predict Result:', y)
+        
+
 def home(request):
-    return HttpResponse("Hello, Django!")
+    first = request.GET['x1']
+    second = request.GET['x2']
+    # requestDict = request.GET
+    # result = int(age02)+5
+    # requestDict = {'result_response':result}
+
+    xxy = xxy1(first,second)    
+
+    requestDict = {'result_response': xxy1.getPredict(xxy) }
+    return JsonResponse(requestDict)
+
+# return HttpResponse("Hello, Django!")
